6a031535edf8600ac0611c3488451ae2e5468dac
Miner <Miner@192.168.0.6>
2018-06-11 00:43:02 +0800

    add

---
+++ SunProj/src/com/nt/test/Add.java
@@ -0,0 +1,15 @@
+package com.nt.test;
+
+public class Add {
+
+	public void add1(int a,int b){
+		int c=a+b;
+		System.out.println(c);
+	}
+	public static void main(String[] args) {
+		Add obj=new Add();
+		obj.add1(10,20);
+
+	}
+
+}
